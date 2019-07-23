f57e956b19cd73ac93daeed8aa86b5115646595a
Ellison Yang <softocean.stuido@gmail.com>
2019-07-23 23:48:42 +0800

    fix apk

--- SimpleChat/app/src/main/java/demo/app/simplechat/ui/MainFragment.java
+++ SimpleChat/app/src/main/java/demo/app/simplechat/ui/MainFragment.java
@@ -68,6 +68,8 @@ public class MainFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        DaggerComponentHolder.getAppComponent().inject(this);
+
         mProfileDialog = new ProfileDialog(getContext());
         if(mLocalRepository.isFirstTimeUse()){
             mLocalRepository.saveFirstTimeUse(false);
